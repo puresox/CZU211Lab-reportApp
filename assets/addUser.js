9316e1ccb4225c6439843e137099a9fc236acbd2
puresox <puresox@163.com>
2021-01-22 09:28:05 +0800

    添加被试

--- assets/addUser.js
+++ assets/addUser.js
@@ -2,18 +2,31 @@
 // It has the same sandbox as a Chrome extension.
 // eslint-disable-next-line import/no-extraneous-dependencies
 const { ipcRenderer } = require('electron');
+const { addUser } = require('../db');
 
-function addUser() {
+function getRadioCheckedValue(name) {
+  const radios = document.getElementsByName(name);
+  for (let i = 0; i < radios.length; i += 1) {
+    if (radios[i].checked) {
+      return radios[i].value;
+    }
+  }
+  return radios[0].value;
+}
+
+async function addUserTodb() {
   const name = document.getElementById('name').value;
   const age = document.getElementById('age').value;
-  const gender = document.getElementById('gender').value;
-  const type = document.getElementById('type').value;
+  const gender = getRadioCheckedValue('gender');
+  const type = getRadioCheckedValue('type');
+  await addUser(name, age, gender, type);
 }
 
 window.addEventListener('DOMContentLoaded', () => {
+  // 监听提交事件
   const addUserBtn = document.getElementById('submit');
-  addUserBtn.addEventListener('click', () => {
-    addUser();
+  addUserBtn.addEventListener('click', async () => {
+    await addUserTodb();
     ipcRenderer.send('close-addUser');
   });
 });
--- assets/index.js
+++ assets/index.js
@@ -2,26 +2,35 @@
 // It has the same sandbox as a Chrome extension.
 // eslint-disable-next-line import/no-extraneous-dependencies
 const { ipcRenderer } = require('electron');
+const { getUsers } = require('../db');
 // 渲染模板
 function renderUserRaws(users) {
   const userRawsArea = document.getElementById('userRawsArea');
   const template = document.getElementById('userRawTemplate'); // 获取template
   const templateContent = template.content; // 获取template里的内容（不包括template）
-  users.forEach((user, index) => {
+  users.forEach((user) => {
     const userRaw = document.importNode(templateContent, true); // 创建模板实列的副本
     const tds = userRaw.querySelectorAll('td');
-    tds[0].textContent = index;
-    tds[1].textContent = user;
+    console.log(userRaw.id);
+    console.log(userRaw.name);
+    console.log(userRaw.querySelector('tr').id);
+    userRaw.querySelector('tr').id = user.id;
+    tds[0].textContent = user.id;
+    tds[1].textContent = user.name;
+    tds[2].textContent = user.age;
+    tds[3].textContent = user.gender;
+    tds[4].textContent = user.type;
     userRawsArea.append(userRaw);
   });
 }
 
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded', async () => {
   // 监听按钮点击事件
   const addUserBtn = document.getElementById('addUser');
   addUserBtn.addEventListener('click', () => {
     ipcRenderer.send('open-addUser');
   });
-
-  renderUserRaws([1, 2, 3]);
+  // 生成用户列表
+  const users = await getUsers();
+  renderUserRaws(users);
 });
